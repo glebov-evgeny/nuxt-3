daf6bd83ce5dd4f9953b211787fdf1c1605e2766
Глебов Евгений Андреевич <EGlebov@synergy.ru>
2023-02-17 14:52:17 +0300

    feat(components): добавил футер
    НОМЕР ЗАДАЧИ: main

--- components/s-footer/s-footer.vue
+++ components/s-footer/s-footer.vue
@@ -1,12 +1,41 @@
 <template>
   <footer class="s-footer">
     <div class="s-footer__container l-wide">
-      <p>aeenth</p>
+      <div class="s-footer__block">
+        <div class="s-footer__box">
+          <p class="s-footer__title">Контакты:</p>
+          <div class="s-footer__box-content">
+            <a href="tel:+79162176557" class="s-footer__link">
+              <span>8 (916) 217-65-57</span>
+            </a>
+            <a href="mailto:egoji@mail.ru" class="s-footer__link">
+              <span>egoji@mail.ru</span>
+            </a>
+          </div>
+        </div>
+        <div class="s-footer__box">
+          <p class="s-footer__title">Полезные ссылки:</p>
+          <div class="s-footer__social">
+            <a href="https://t.me/eaglebov" class="s-footer__social-link" target="_blank">
+              <img class="s-footer__social-image" src="/images/icons/social/tg.svg" alt="social-link" />
+            </a>
+            <a href="https://wa.me/79162176557" class="s-footer__social-link" target="_blank">
+              <img class="s-footer__social-image" src="/images/icons/social/wa.svg" alt="social-link" />
+            </a>
+          </div>
+        </div>
+      </div>
+      <p class="s-footer__rights">{{ getYear }} © Разработка, продвижение, etc.</p>
     </div>
   </footer>
 </template>
 
-<script setup></script>
+<script setup>
+const getYear = computed(() => {
+  const currentYear = new Date();
+  return currentYear.getFullYear();
+});
+</script>
 
 <style lang="scss">
 @import './s-footer.scss';
